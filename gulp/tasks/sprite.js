0b5b137c5be1c3017071fb446d87043d2e929349
Oleg Unger <ounger@inbox.ru>
2016-12-01 04:40:21 +0300

    two pages created

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -3,13 +3,13 @@
 module.exports = function() {
   $.gulp.task('sprite', function() {
 
-    var spriteData = $.gulp.src('./source/images/sprite/*.png')
+    var spriteData = $.gulp.src('./source/images/picForSprites/*.png')
 	.pipe($.gp.spritesmith({
 		imgName: 'sprite.png',
 		cssName: 'sprite.scss',
 		padding: 5,
 		algorithm: 'left-right'
 	}));
-	return spriteData.pipe($.gulp.dest('./source/images/sprites'));
+	return spriteData.pipe($.gulp.dest($.config.root + '/assets/img/sprites'));
   });
 };
