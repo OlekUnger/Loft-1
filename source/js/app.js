7fbb79c80c7804db38249e3e265d9e708f48daf8
Oleg Unger <ounger@inbox.ru>
2016-12-03 17:47:03 +0300

    -

--- source/js/app.js
+++ source/js/app.js
@@ -4,4 +4,18 @@
   setTimeout(function() {
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
+
+
 })();
+
+// $(document).ready(function(){
+
+//   $(".menuButton").click(function(e){
+//     e.preventDefault();
+  
+//     $(this).toggleClass('active');
+//     // $(".navMenu").fadeToggle('slow');
+    
+//   });    
+
+// });
