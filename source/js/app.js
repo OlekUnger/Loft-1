9b97f4cd2828667dd73c32b92ce1132396aa2017
Oleg Unger <ounger@inbox.ru>
2016-12-11 05:50:44 +0300

    -

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,6 @@
+
+// hamburger
+
 (function(){
 	$(".menuButton").click(function(e){
 		e.preventDefault();
@@ -5,6 +8,8 @@
 	});
 })();
 
+
+// flip
 (function(){
 	$(".autorize_btn").click(function(e){
 		e.preventDefault();
@@ -12,21 +17,69 @@
 	})
 })();
 
+(function(){
+	$(".blur").draggable();
+});
+
+// parallax
+
+var parallax = (function(){
+	var bg2 = document.querySelector(".layer-2");
+	var user = document.querySelector(".blockUser-wrap");
+	var headerWrap = document.querySelector(".header_wrap");
+
+	return {
+		move: function (block, windowScroll, strafeAmount) {
+			var strafe = windowScroll/strafeAmount + '%';
+			var tarnsformString = 'translate3d(0, '+strafe+', 0)';
+			var style = block.style;
+
+			style.top =strafe;
+			style.transform = tarnsformString;
+			style.webkitTransform = tarnsformString;
+		},
+		init: function(wScroll) {
+			this.move( bg2, wScroll, 85),
+			this.move(user, wScroll, -50);
+
+		}
+	}
+}());
 
 
-// var openMenu = document.querySelector(".menuButton");
-// var	autorizeBtn = document.querySelector(".autorize_btn");
-// var	flipContainer = document.querySelector(".flipContainer");
 
-// 	openMenu.addEventListener('click', function(event){
-//   			event.preventDefault();
-//   			this.classList.toggle('active');
-// 	});
+// blur
 
-// 	autorizeBtn.addEventListener('click', function(event){
-// 		event.preventDefault();
-// 		flipContainer.classList.toggle('flip');
-// 	});	
+// var blur = (function() {
+// 	var wrapper = document.querySelector(".blur_bg");
+// 	var blurBg = document.querySelector(".blur_bg");
+
+// 	return {
+// 		set: function(){
+// 			var imgWidth = document.querySelector(".talkAbout").offsetWidth,
+// 			posLeft = -wrapper.offsetLeft,
+// 			posTop = -wrapper.offsetTop,
+// 			blurCss = form.style;
+
+// 			blurCss.backgroundSize = imgWidth + 'px' + '' + auto;
+// 			blurCss.backgroundPosition = posLeft + 'px' + '' + posTop + 'px';
+
+// 		}
+// 	}
+// }());
+
+// blur.set();
+
+
+window.onscroll = function(){
+	var wScroll = window.pageYOffset;
+	parallax.init(wScroll);
+}
+
+window.onresize = function(){
+	blur.set();
+}
+
 
 
 
